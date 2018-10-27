a36994e83f44e34119f4c12bd8ac329af6de99eb
JAKOB\Jakob <jakob.ahrneteg@gmail.com>
2018-10-27 13:28:38 +0200

    More work on scene management

--- Paradox/src/System/Scene/Scene.cpp
+++ Paradox/src/System/Scene/Scene.cpp
@@ -1,26 +1,82 @@
 #include <System/Scene/Scene.hpp>
 
+// C++
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+
+// SFML
+//#include <SFML/Graphics/RenderTarget.hpp>
+
+// Paradox
+#include <System/Logic/TransformSystem.hpp>
+#include <System/Logic/RenderSystem.hpp>
+
+// Json
+#include <json/json.hpp>
+
+using namespace nlohmann;
+
 namespace paradox
 {
 	Scene::Scene() :
-	m_name("Untitled")
+	m_name("Untitled"),
+	m_transformSystem(std::make_unique<TransformSystem>()),
+	m_renderSystem(std::make_unique<RenderSystem>())
 	{
-		// Load scene?
 	}
 
 	Scene::~Scene()
 	{
-		// Unload scene?
+		m_transformSystem.reset();
+		m_renderSystem.reset();
 	}
 
-	void Scene::loadScene()
+	void Scene::loadScene(const std::string& path)
 	{
-		// Loop over all entities in the current scene file and create them?
+		// Cache the path for future need?
+		json data;
+		std::ifstream handle;
+		std::ios_base::iostate exceptionMask = handle.exceptions() | std::ios::failbit;
+		handle.exceptions(exceptionMask);
+
+		try
+		{
+			handle.open(path);
+			handle >> data;
+			handle.close();
+		}
+		catch (const std::ios_base::failure& e)
+		{
+			std::cerr << "Error: Failed reading " << path << " -> " << std::strerror(errno) << std::endl; // TODO: log to game console
+		}
+
+		if (!data.empty())
+		{
+			// TODO: create entities from the scene data	
+		}
+	}
+
+	void Scene::saveScene()
+	{
+		// Any ability in entt to serialize entities directly?
+
+		// TODO: save all the scene data to disk
 	}
 
 	void Scene::unloadScene()
 	{
 		// Loop over all entities in the current scene file and destroy them?
+	}
+
+	void Scene::update()
+	{
+		m_transformSystem->update(m_entities);
+	}
+
+	void Scene::draw(sf::RenderTarget& target)
+	{
+		m_renderSystem->update(m_entities, target);
 	}
 
 	void Scene::setName(const std::string& name)
--- Paradox/src/System/Scene/Scene.hpp
+++ Paradox/src/System/Scene/Scene.hpp
@@ -3,24 +3,38 @@
 // Entt
 #include <entt/entity/registry.hpp>
 
+// SFML
+namespace sf
+{
+	class RenderTarget;
+}
+
 namespace paradox
 {
+	// Paradox
+	class TransformSystem;
+	class RenderSystem;
+
 	class Scene
 	{
 	public:
 		Scene();
 		~Scene();
 
-		// Scene manager?
-		void loadScene();
-		void unloadScene();
-		//
+		// TODO: ability to add entities to the scene
 
+		void loadScene(const std::string& path);
+		void saveScene();
+		void unloadScene();
+		void update();
+		void draw(sf::RenderTarget& target);
 		void setName(const std::string& name);
 		std::string getName() const;
 
 	private:
 		entt::registry<unsigned> m_entities;
 		std::string m_name;
+		std::unique_ptr<TransformSystem> m_transformSystem;
+		std::unique_ptr<RenderSystem> m_renderSystem;
 	};
 }
